da675c3f72af8111c840d8e086e67f863a2a7a4e
luoheng <1301089462@qq.com>
2019-07-07 19:39:12 +0800

    maxheap and minheap ok

--- chapter6/heap.go
+++ chapter6/heap.go
@@ -1,5 +1,7 @@
 package chapter6
 
+import "fmt"
+
 type heap struct {
 	A        []int
 	heapSize int
@@ -74,4 +76,82 @@ func HeapSort(A []int) {
 		h.heapSize--
 		h.maxHeapify(0)
 	}
+}
+
+// maximum priority queue
+// method:
+// InsertMax
+// Maximum
+// ExtractMax
+// IncreaseKey
+func (h *heap) InsertMax(x int) {
+	h.heapSize++
+	h.A[h.heapSize-1] = x - 1
+	h.IncreaseKey(h.heapSize-1, x)
+}
+
+func (h *heap) Maximum() int {
+	return h.A[0]
+}
+
+func (h *heap) ExtractMax() (int, error) {
+	if h.heapSize < 1 {
+		return 0, fmt.Errorf("heap overflow")
+	}
+	max := h.A[0]
+	h.A[0] = h.A[h.heapSize-1]
+	h.heapSize--
+	h.maxHeapify(0)
+	return max, nil
+}
+
+func (h *heap) IncreaseKey(i int, key int) error {
+	if key < h.A[i] {
+		return fmt.Errorf("new key is smaller than current key")
+	}
+	h.A[i] = key
+	for i > 0 && h.A[parent(i)] < h.A[i] {
+		h.A[i], h.A[parent(i)] = h.A[parent(i)], h.A[i]
+		i = parent(i)
+	}
+	return nil
+}
+
+// minimum priority queue
+// method:
+// Minimum
+// ExtractMin
+// DecreaseKey
+// InsertMin
+func (h *heap) Minimum() int {
+	return h.A[0]
+}
+
+func (h *heap) ExtractMin() (int, error) {
+	if h.heapSize < 1 {
+		return 0, fmt.Errorf("heap overflow")
+	}
+	min := h.A[0]
+	h.A[0] = h.A[h.heapSize-1]
+	h.heapSize--
+	h.minHeapify(0)
+	return min, nil
+}
+
+func (h *heap) DecreaseKey(i int, key int) error {
+	if key > h.A[i] {
+		return fmt.Errorf("new key is bigger than current key")
+	}
+	h.A[i] = key
+	for i > 0 && h.A[parent(i)] > h.A[i] {
+		h.A[i], h.A[parent(i)] = h.A[parent(i)], h.A[i]
+		i = parent(i)
+	}
+	return nil
+}
+
+func (h *heap) InsertMin(x int) {
+	h.heapSize++
+	h.A[h.heapSize-1] = x + 1
+	h.DecreaseKey(h.heapSize-1, x)
 }
