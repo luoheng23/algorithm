c189d3db650a5db9b9d471237b6b2bb6976ee576
luoheng <1301089462@qq.com>
2019-07-07 09:52:42 +0800

    insertion sort decrease

--- chapter1/insertion_sort.go
+++ chapter1/insertion_sort.go
@@ -10,3 +10,14 @@ func InsertionSort(A []int) {
 		A[i+1] = key
 	}
 }
+
+func InsertionSortDecr(A []int) {
+	for j := 1; j < len(A); j++ {
+		key, i := A[j], j-1
+		for i >= 0 && A[i] < key {
+			A[i+1] = A[i]
+			i--
+		}
+		A[i+1] = key
+	}
+}
--- chapter1/insertion_sort_test.go
+++ chapter1/insertion_sort_test.go
@@ -11,6 +11,15 @@ func TestInsertionSort(t *testing.T) {
 	}
 }
 
+func TestInsertionSortDecr(t *testing.T) {
+	s := [10]int{5, 3, 7, 8, 4, 6, 0, 9, 2, 1}
+	InsertionSortDecr(s[:])
+	sortedS := [10]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	if s != sortedS {
+		t.Errorf("InsertionSort failed. Expected %v, Got %v", sortedS, s)
+	}
+}
+
 func BenchmarkInsertionSort(b *testing.B) {
 	var s [100]int
 	for i := 100; i > 0; i-- {
