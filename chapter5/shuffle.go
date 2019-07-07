a5aa673459eabbd4573f5cbc3c80308e75c779ca
luoheng <1301089462@qq.com>
2019-07-07 18:09:14 +0800

    correct array shuffle

--- chapter5/shuffle.go
+++ chapter5/shuffle.go
@@ -9,7 +9,7 @@ func Shuffle(A []int) {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	len := len(A)
 	for i, _ := range A {
-		j := r.Intn(len)
+		j := r.Intn(len-i) + i
 		A[i], A[j] = A[j], A[i]
 	}
 }
