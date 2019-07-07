dcdde384db31e81f2d233f10d98a76cf1dbb29b7
luoheng <1301089462@qq.com>
2019-07-07 15:48:44 +0800

    benchmark for maxSubArray merge and line version

--- chapter4/max_sub_array_test.go
+++ chapter4/max_sub_array_test.go
@@ -11,11 +11,24 @@ func TestMaxSubArray(t *testing.T) {
 	}
 }
 
+func BenchmarkMaxSubArray(b *testing.B) {
+	var array = [16]int{13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 28, -8, 7}
+	for i := 0; i < b.N; i++ {
+		MaxSubArray(array[:], 0, len(array))
+	}
+}
+
 func TestMaxSubArrayLine(t *testing.T) {
 	var array = [16]int{13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 28, -8, 7}
 	left, right, sum := 7, 13, 44
 	l, r, s := MaxSubArrayLine(array[:])
 	if l != left || r != right || s != sum {
 		t.Errorf("MaxSubArrayLine failed. Expected (%d, %d, %d), Got (%d, %d, %d).", left, right, sum, l, r, s)
+	}
+}
+func BenchmarkMaxSubArrayLine(b *testing.B) {
+	var array = [16]int{13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 28, -8, 7}
+	for i := 0; i < b.N; i++ {
+		MaxSubArrayLine(array[:])
 	}
 }
