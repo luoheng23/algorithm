70722616ed4b693f3d5c04dc072a4e6475fcd73a
luoheng <1301089462@qq.com>
2019-07-08 13:14:38 +0800

    add test to maxheap and minheap

--- chapter6/heap_test.go
+++ chapter6/heap_test.go
@@ -10,3 +10,31 @@ func TestHeapSort(t *testing.T) {
 		t.Errorf("HeapSort failed. Expected %v, Got %v.", sortedS, s)
 	}
 }
+
+func TestMaxHeap(t *testing.T) {
+	s := [9]int{5, 13, 2, 25, 7, 17, 20, 8, 4}
+	sortedS := [9]int{25, 20, 17, 13, 8, 7, 5, 4, 2}
+	newS := [9]int{}
+	h := BuildMaxHeap(s[:])
+	for i := 0; i < 9; i++ {
+		newS[i], _ = h.ExtractMax()
+	}
+	if newS != sortedS {
+		t.Errorf("MaxHeap failed. Expected %v, Got %v.", sortedS, newS)
+	}
+
+}
+
+func TestMinHeap(t *testing.T) {
+	s := [9]int{5, 13, 2, 25, 7, 17, 20, 8, 4}
+	sortedS := [9]int{2, 4, 5, 7, 8, 13, 17, 20, 25}
+	newS := [9]int{}
+	h := BuildMinHeap(s[:])
+	for i := 0; i < 9; i++ {
+		newS[i], _ = h.ExtractMin()
+	}
+	if newS != sortedS {
+		t.Errorf("MaxHeap failed. Expected %v, Got %v.", sortedS, newS)
+	}
+
+}
