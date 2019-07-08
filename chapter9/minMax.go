2d9fe93c1b6762c849df4cc8c07db7eb6f0f6d37
luoheng <1301089462@qq.com>
2019-07-08 14:29:14 +0800

    MinMax ok

--- chapter9/minMax.go
+++ chapter9/minMax.go
@@ -25,3 +25,25 @@ func Max(A []int) int {
 	}
 	return max
 }
+
+func MinMax(A []int) (min int, max int) {
+	if len(A) < 1 {
+		return 0, 0
+	}
+	min, max = A[0], A[0]
+	mi, ma := 0, 0
+	for i := 1; i < len(A)-1; i += 2 {
+		if A[i] > A[i+1] {
+			mi, ma = A[i+1], A[i]
+		} else {
+			mi, ma = A[i], A[i+1]
+		}
+		if ma > max {
+			max = ma
+		}
+		if mi < min {
+			min = mi
+		}
+	}
+	return min, max
+}
