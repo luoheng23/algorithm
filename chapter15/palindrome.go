3f82e225be37332e3d1aeddf9a95b637cfd9e709
luoheng <1301089462@qq.com>
2019-07-16 13:20:47 +0800

    dynamic problem

--- chapter15/palindrome.go
+++ chapter15/palindrome.go
@@ -1,8 +1,6 @@
 package chapter15
 
-import (
-	"strings"
-)
+import "strings"
 
 // add # between strings
 func convert(p string) string {
@@ -13,42 +11,40 @@ func unConvert(p string) string {
 	return strings.Join(strings.Split(p, "#"), "")
 }
 
-// PalindromeSlow solve palindrome problem
+// PalindromeSlow O(n^2)
 func PalindromeSlow(p string) string {
-	cP := convert(p)
-	length := len(cP)
+	length := len(p)
 	maxI, maxL := 0, 0
 	for i := 0; i < length; i++ {
-		j, k := i, i
-		for j >= 0 && k < length && cP[j] == cP[k] {
+		j, k := i-1, i+1
+		for j >= 0 && k < length && p[j] == p[k] {
 			j--
 			k++
 		}
-		if maxL < k-j+1 {
+		if maxL < k-j-1 {
 			maxI = i
 			maxL = k - j - 1
 		}
 	}
-	return unConvert(cP[maxI-maxL/2 : maxI+maxL/2+1])
+	return p[maxI-maxL/2 : maxI+maxL/2+1]
 }
 
-// PalindromeFast solve palindrome problem
+// PalindromeFast O(n)
 func PalindromeFast(p string) string {
-	cP := convert(p)
-	length := len(cP)
+	length := len(p)
 	s := make([]int, length)
 	id, mx := 0, 0
 	for i := 0; i < length; i++ {
-		if mx > i {
-			if mx-i >= s[2*id-i] {
-				s[i] = s[2*id-i]
-			} else {
-				s[i] = mx - i
-			}
+		if mx > i && mx-i > s[2*id-i] {
+			s[i] = s[2*id-i]
+			continue
+		}
+		if mx-i > 1 {
+			s[i] = mx - i
 		} else {
 			s[i] = 1
 		}
-		for i-s[i] >= 0 && i+s[i] < length && cP[i+s[i]] == cP[i-s[i]] {
+		for i-s[i] >= 0 && i+s[i] < length && p[i+s[i]] == p[i-s[i]] {
 			s[i]++
 		}
 		if s[i]+i > mx {
@@ -61,25 +57,20 @@ func PalindromeFast(p string) string {
 			maxI, maxL = i, a
 		}
 	}
-	return unConvert(cP[maxI-maxL+1 : maxI+maxL])
-
+	return p[maxI-maxL+1 : maxI+maxL]
 }
 
-// func PalindromeVeryFast(p string) string {
-// 	cP := convert(p)
-// 	length := len(cP)
-// 	maxI, maxL, lastI := 0, 0, 0
-// 	for i := 1; i < length; i++ {
-// 		if 2*lastI-i < 0 || cP[2*lastI-i] != cP[i] {
-// 			if i-2 >= 0 && cP[i-2] == cP[i] {
-// 				lastI = i - 1
-// 			} else {
-// 				lastI = i
-// 			}
-// 		}
-// 		if i-lastI > maxL {
-// 			maxI, maxL = lastI, i-lastI
-// 		}
-// 	}
-// 	return unConvert(cP[maxI-maxL : maxI+maxL+1])
-// }
+// PalindromeVeryFast dynamic programming O(n)
+func PalindromeVeryFast(p string) string {
+	length := len(p)
+	maxI, maxL, id := 0, 0, 0
+	for i := 1; i < length; i++ {
+		for 2*id-i < 0 || p[2*id-i] != p[i] {
+			id++
+		}
+		if i-id > maxL {
+			maxI, maxL = id, i-id
+		}
+	}
+	return p[maxI-maxL : maxI+maxL+1]
+}
--- chapter15/palindrome_test.go
+++ chapter15/palindrome_test.go
@@ -6,52 +6,52 @@ import (
 )
 
 func TestPalindromeSlow(t *testing.T) {
-	s1 := "caracter"
-	s2 := "ttccabbaccd"
-	if PalindromeSlow(s1) != "carac" || PalindromeSlow(s2) != "ccabbacc" {
-		t.Errorf("PalindromeSlow failed. Expected (carac, ccabbacc), Got (%s, %s)", PalindromeSlow(s1), PalindromeSlow(s2))
+	s1 := convert("caracter")
+	s2 := convert("ttccabbaccd")
+	if unConvert(PalindromeSlow(s1)) != "carac" || unConvert(PalindromeSlow(s2)) != "ccabbacc" {
+		t.Errorf("PalindromeSlow failed. Expected (carac, ccabbacc), Got (%s, %s)", unConvert(PalindromeSlow(s1)), unConvert(PalindromeSlow(s2)))
 	}
 }
 
 func BenchmarkPalindromeSlow(b *testing.B) {
 	s := "abcdefafadsafasdfsdfsccdfhzhvxzjkhfkfhskdhfjsfewufiwe"
-	s = strings.Repeat(s, 200)
+	s = convert(strings.Repeat(s, 200) + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 	for i := 0; i < b.N; i++ {
 		PalindromeSlow(s)
 	}
 }
 
 func TestPalindromeFast(t *testing.T) {
-	s1 := "caracter"
-	s2 := "ttccabbaccd"
-	if PalindromeFast(s1) != "carac" || PalindromeFast(s2) != "ccabbacc" {
-		t.Errorf("PalindromeFast failed. Expected (carac, ccabbacc), Got (%s, %s)", PalindromeFast(s1), PalindromeFast(s2))
+	s1 := convert("caracter")
+	s2 := convert("ttccabbaccd")
+	if unConvert(PalindromeFast(s1)) != "carac" || unConvert(PalindromeFast(s2)) != "ccabbacc" {
+		t.Errorf("PalindromeFast failed. Expected (carac, ccabbacc), Got (%s, %s)", unConvert(PalindromeFast(s1)), unConvert(PalindromeFast(s2)))
 	}
 }
 
 func BenchmarkPalindromeFast(b *testing.B) {
 	s := "abcdefafadsafasdfsdfsccdfhzhvxzjkhfkfhskdhfjsfewufiwe"
-	s = strings.Repeat(s, 200)
+	s = convert(strings.Repeat(s, 200) + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") 
 	for i := 0; i < b.N; i++ {
 		PalindromeFast(s)
 	}
 }
 
-// func TestPalindromeVeryFast(t *testing.T) {
-// 	// s1 := "caracter"
-// 	// s2 := "ttccabbaccd"
-// 	// s1 := "abcdefghij"
-// 	s2 := "aabcdefgc"
-// 	s1 := "abacabacaba"
-// 	if PalindromeVeryFast(s1) != "abacabacaba" || PalindromeVeryFast(s2) != "aa" {
-// 		t.Errorf("PalindromeVeryFast failed. Expected (abacabacaba, aa), Got (%s, %s)", PalindromeVeryFast(s1), PalindromeVeryFast(s2))
-// 	}
-// }
+func TestPalindromeVeryFast(t *testing.T) {
+	// s1 := "caracter"
+	// s2 := "ttccabbaccd"
+	// s1 := "abcdefghij"
+	s2 := convert("abcbaba")
+	s1 := convert("abacabacaba")
+	if unConvert(PalindromeVeryFast(s1)) != "abacabacaba" || unConvert(PalindromeVeryFast(s2)) != "abcba" {
+		t.Errorf("PalindromeVeryFast failed. Expected (abacabacaba, aa), Got (%s, %s)", unConvert(PalindromeVeryFast(s1)), unConvert(PalindromeVeryFast(s2)))
+	}
+}
 
-// func BenchmarkPalindromeVeryFast(b *testing.B) {
-// 	s := "abcdefafadsafasdfsdfsccdfhzhvxzjkhfkfhskdhfjsfewufiwe"
-// 	s = strings.Repeat(s, 200)
-// 	for i := 0; i < b.N; i++ {
-// 		PalindromeVeryFast(s)
-// 	}
-// }
+func BenchmarkPalindromeVeryFast(b *testing.B) {
+	s := "abcdefafadsafasdfsdfsccdfhzhvxzjkhfkfhskdhfjsfewufiwe"
+	s = convert(strings.Repeat(s, 200) + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+	for i := 0; i < b.N; i++ {
+		PalindromeVeryFast(s)
+	}
+}
