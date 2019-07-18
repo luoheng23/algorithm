cc2faefd15c0e70138fabd9a36d9ff8d6887bf35
luoheng <1301089462@qq.com>
2019-07-18 14:01:17 +0800

    btree ok

--- chapter18/btree.go
+++ chapter18/btree.go
@@ -2,10 +2,10 @@ package chapter18
 
 // BNode for BTree
 type BNode struct {
-	n int  // number of keys
-	keys []int
-	leaf bool // leaf or not
-	c []*BNode // children (n+1)
+	n    int // number of keys
+	keys []rune
+	leaf bool     // leaf or not
+	c    []*BNode // children (n+1)
 }
 
 // BTree is b tree
@@ -15,21 +15,33 @@ type BTree struct {
 }
 
 func diskRead(x *BNode, c *BNode) {}
-func diskWrite(x *BNode) {}
+func diskWrite(x *BNode)          {}
 
-// Create a BTree
-func Create() BTree {
-	return BTree{
-		&BNode{leaf: true, n: 0}
+// CreateNode return a node
+func (b *BTree) CreateNode() *BNode {
+	return &BNode{
+		n:    0,
+		keys: make([]rune, 2*b.t-1),
+		leaf: true,
+		c:    make([]*BNode, 2*b.t),
 	}
 }
 
-// Search for key
-func (b *BTree) Search(x *BNode, k int) (*BNode, int){
+// CreateTree returns a BTree
+func CreateTree(t int) BTree {
+	b := BTree{}
+	b.t = t
+	b.root = b.CreateNode()
+	return b
+}
+
+// Search for keys
+func (b *BTree) Search(x *BNode, k rune) (*BNode, rune) {
 	i := 0
-	for i = 0; i < x.n && k > x.keys[i]; i++ {}
+	for i = 0; i < x.n && k > x.keys[i]; i++ {
+	}
 	if i < x.n && k == x.keys[i] {
-		return x, i
+		return x, x.keys[i]
 	} else if x.leaf {
 		return nil, 0
 	}
@@ -39,41 +51,42 @@ func (b *BTree) Search(x *BNode, k int) (*BNode, int){
 
 func (b *BTree) splitChild(x *BNode, i int) {
 	y := x.c[i]
-	z := &BNode{leaf: y.leaf, n: b.t-1}
-	z.key = y.key[b.t:]
+	z := b.CreateNode()
+	copy(z.keys, y.keys[b.t:y.n])
 	if !y.leaf {
-		z.c = y.c[b.t:]
+		copy(z.c, y.c[b.t:y.n+1])
 	}
-	y.n -= t - 1
-	x.c = append(x.c, nil)
-	x.c[i+2:] = x.c[i+1:x.n+1]
-	x.c[i+1] = z
-	x.keys = append(x.keys, nil)
-	x.keys[i+1:] = x.keys[i:x.n]
-	x.key[i] = y.key[i]
+	z.n = b.t - 1
+	y.n = b.t - 1
 	x.n++
+	copy(x.c[i+2:x.n+1], x.c[i+1:x.n])
+	x.c[i+1] = z
+	copy(x.keys[i+1:x.n], x.keys[i:x.n-1])
+	x.keys[i] = y.keys[y.n]
 	diskWrite(y)
 	diskWrite(z)
 	diskWrite(x)
 }
 
-func (b *BTree) Insert(k int) {
+// Insert a node
+func (b *BTree) Insert(k rune) {
 	r := b.root
-	if r.n == 2 * b.t - 1 {
-		s := &BNode{leaf: false, n: 0}
-		s.c = make([]int, 4)
+	if r.n == 2*b.t-1 {
+		s := b.CreateNode()
+		b.root = s
 		s.c[0] = r
-		b.splitChild(s, 1)
+		s.leaf = false
+		b.splitChild(s, 0)
 		b.insertNotFull(s, k)
 	} else {
 		b.insertNotFull(r, k)
 	}
 }
 
-func (b *BTree) insertNotFull(x *BNode, k int) {
-	i := x.n
+func (b *BTree) insertNotFull(x *BNode, k rune) {
+	i := x.n - 1
 	if x.leaf {
-		for i >= 0 && k < x.key[i] {
+		for i >= 0 && k < x.keys[i] {
 			x.keys[i+1] = x.keys[i]
 			i--
 		}
@@ -81,17 +94,81 @@ func (b *BTree) insertNotFull(x *BNode, k int) {
 		x.n++
 		diskWrite(x)
 	} else {
-		for i >= 0 && k < x.key[i] {
+		for i >= 0 && k < x.keys[i] {
 			i--
 		}
 		i++
-		diskRead(x, c[i])
+		diskRead(x, x.c[i])
 		if x.c[i].n == 2*b.t-1 {
 			b.splitChild(x, i)
-			if k > x.key[i] {
+			if k > x.keys[i] {
 				i++
 			}
 		}
 		b.insertNotFull(x.c[i], k)
 	}
+}
+
+// Delete a node
+func (b *BTree) Delete(x *BNode, k rune){
+	i := 0
+	for ; i < x.n; i++ {
+		if x.keys[i] >= k {
+			break
+		}
+	}
+	if i < x.n && x.keys[i] == k {
+		if x.leaf {
+			copy(x.keys[i:], x.keys[i+1:])
+			x.n--
+		} else if x.c[i].n >= b.t {
+			x.keys[i] = x.c[i].keys[x.c[i].n-1]
+			b.Delete(x.c[i], x.keys[i])
+		} else if x.c[i+1].n >= b.t {
+			x.keys[i] = x.c[i+1].keys[0]
+			b.Delete(x.c[i+1], x.keys[i])
+		} else {
+			x.c[i].keys[x.c[i].n] = x.keys[i]
+			x.c[i].n++
+			copy(x.c[i].keys[x.c[i].n:], x.c[i+1].keys[:x.c[i+1].n])
+			x.c[i].n += x.c[i+1].n
+			copy(x.c[i].c[x.n+1:], x.c[i+1].c[:x.c[i+1].n+1])
+			copy(x.keys[i:], x.keys[i+1:])
+			copy(x.c[i+1:], x.c[i+2:])
+			x.n--
+			b.Delete(x.c[i], k)
+		}
+	} else if i < x.n && x.keys[i] > k && !x.leaf {
+		if x.c[i].n < b.t {
+			x.c[i].keys[x.c[i].n] = x.keys[i]
+			x.c[i].n++
+			if x.c[i+1].n >= b.t {
+				x.keys[i] = x.c[i+1].keys[0]
+				b.Delete(x.c[i+1], x.keys[i])
+				b.Delete(x.c[i], k)
+				} else {
+				copy(x.c[i].keys[x.c[i].n:], x.c[i+1].keys[:x.c[i+1].n])
+				x.c[i].n += x.c[i+1].n
+				copy(x.c[i].c[x.n+1:], x.c[i+1].c[:x.c[i+1].n+1])
+				copy(x.keys[i:], x.keys[i+1:])
+				copy(x.c[i+1:], x.c[i+2:])
+				x.n--
+			}
+		}
+		b.Delete(x.c[i], k)
+	}
+}
+
+func (b *BTree) inOrder(s *BNode) []rune {
+	A := make([]rune, 0, 4)
+	if s != nil {
+		for i, key := range s.keys {
+			if i < s.n {
+				A = append(A, b.inOrder(s.c[i])...)
+				A = append(A, key)
+			}
+		}
+		A = append(A, b.inOrder(s.c[s.n])...)
+	}
+	return A
 }
