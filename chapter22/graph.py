787ba9b882589b447dba0a3b5573326fb4be62b8
luoheng <1301089462@qq.com>
2019-07-22 19:28:58 +0800

    graph in python

--- chapter22/graph.py
+++ chapter22/graph.py
@@ -1,14 +1,42 @@
 
-class vertex(int):
+from collections import defaultdict
+
+
+class Vertex(int):
+
+    def __init__(self, value):
+        self.color = Graph.WHITE
+        self.d = float("inf")
+        self.pi = None
+
+
+class Graph:
     WHITE = 0
     GRAY = 1
     BLACK = 2
 
-    def __init__(self, value):
-        self.color = vertex.WHITE
-        self.d = float("inf")
-        self.pi = None
-
-class Graph:
     def __init__(self, vertex, edges):
-        self.V = 
+        self.V = {num: Vertex(num) for num in vertex}
+        self.Adj = defaultdict(list)
+        for u, v in edges:
+            self.Adj[u].append(v)
+            self.Adj[v].append(u)
+
+    def __str__(self):
+        return f"{self.V}, {self.Adj}"
+
+
+def initGraph(vertex, edges):
+    return Graph(vertex, edges)
+
+
+def main():
+    vertex = [0, 1, 2, 3, 4, 5, 6, 7]
+    edges = [(0, 1), (1, 2), (2, 3), (3, 4), (3, 5),
+             (4, 5), (4, 7), (4, 6), (5, 6), (6, 7)]
+    graph = initGraph(vertex, edges)
+    print(graph)
+
+
+if __name__ == "__main__":
+    main()
