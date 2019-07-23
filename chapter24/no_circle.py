b4a820f6d3eef06b4bd985fa00dfce1af38ec936
luoheng <1301089462@qq.com>
2019-07-23 15:48:29 +0800

    format code

--- chapter24/no_circle.py
+++ chapter24/no_circle.py
@@ -1,9 +1,9 @@
 
 import sys
 sys.path.insert(0, '..')
-
 from chapter22.graph import initGraph
 from relax import init, relax
+
 
 WHITE = 0
 GRAY = 1
@@ -11,6 +11,7 @@ BLACK = 2
 
 time = 0
 L = []
+
 
 def topoSort(G):
     global time, L
@@ -21,6 +22,7 @@ def topoSort(G):
     for v in G.V:
         if G.V[v].color == WHITE:
             dfsVisit(G, v)
+
 
 def dfsVisit(G, v):
     global time
@@ -36,6 +38,7 @@ def dfsVisit(G, v):
     G.V[v].f = time
     L.insert(0, v)
 
+
 def noCircle(G, s):
     topoSort(G)
     init(G, s)
@@ -43,15 +46,17 @@ def noCircle(G, s):
         for v in G.Adj[u]:
             relax(G, u, v)
 
+
 def main():
     vertex = ['r', 's', 't', 'y', 'x', 'z']
     edges = [('r', 's', 5), ('r', 't', 3), ('s', 'x', 6), ('s', 't', 2),
-            ('t', 'y', 4), ('t', 'z', 2), ('t', 'x', 7), ('x', 'y', -1),
-            ('x', 'z', 1), ('y', 'z', -2)]
+             ('t', 'y', 4), ('t', 'z', 2), ('t', 'x', 7), ('x', 'y', -1),
+             ('x', 'z', 1), ('y', 'z', -2)]
     G = initGraph(vertex, edges)
     noCircle(G, 's')
     for v in G.V:
         print(G.V[v], G.V[v].d)
+
 
 if __name__ == "__main__":
     main()
