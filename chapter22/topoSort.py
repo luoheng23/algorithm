cdf9a71f90514b31f4146823c98b41f828f979e9
luoheng <1301089462@qq.com>
2019-07-23 15:32:22 +0800

    fix color problem

--- chapter22/topoSort.py
+++ chapter22/topoSort.py
@@ -1,30 +1,38 @@
 
 from graph import initGraph
 
+WHITE = 0
+GRAY = 1
+BLACK = 2
 time = 0
 L = []
+
 
 def topoSort(G):
     global time, L
     time = 0
     L = []
     for v in G.V:
-        if G.V[v].color == G.WHITE:
+        G.V[v].color = WHITE
+    for v in G.V:
+        if G.V[v].color == WHITE:
             dfsVisit(G, v)
+
 
 def dfsVisit(G, v):
     global time
     time += 1
     G.V[v].d = time
-    G.V[v].color = G.GRAY
+    G.V[v].color = GRAY
     for u in G.Adj[v]:
-        if G.V[u].color == G.WHITE:
+        if G.V[u].color == WHITE:
             G.V[u].pi = v
             dfsVisit(G, u)
-    G.V[v].color = G.BLACK
+    G.V[v].color = BLACK
     time += 1
     G.V[v].f = time
     L.insert(0, v)
+
 
 def main():
     vertex = [0, 1, 2, 3, 4, 5, 6, 7]
@@ -34,6 +42,6 @@ def main():
     topoSort(G)
     print(L)
 
+
 if __name__ == "__main__":
     main()
-
