202916b8ee41d25850ddb7d0399654f3adf2b8c3
luoheng <1301089462@qq.com>
2019-08-27 19:12:16 +0800

    binary search

--- c-learning-algorithm/chapter3/search.go
+++ c-learning-algorithm/chapter3/search.go
@@ -9,3 +9,19 @@ func Search(A []int, value int) int {
 	}
 	return -1
 }
+
+// BinarySearch for value in sorted A
+func BinarySearch(A []int, value int) int {
+	low, high := 0, len(A)-1
+	for low < high {
+		mid := (low + high) / 2
+		if A[mid] > value {
+			high = mid
+		} else if A[mid] < value {
+			low = mid + 1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
--- c-learning-algorithm/chapter3/search_test.go
+++ c-learning-algorithm/chapter3/search_test.go
@@ -20,3 +20,22 @@ func BenchmarkSearch(b *testing.B) {
 		Search(A, n)
 	}
 }
+
+func TestBinarySearch(t *testing.T) {
+	A := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	value1, value2 := 9, 20
+	if BinarySearch(A, value1) != 9 || BinarySearch(A, value2) != -1 {
+		t.Errorf("BinarySearch failed. Expect (%d, %d), Got (%d, %d).", 9, -1, BinarySearch(A, value1), BinarySearch(A, value2))
+	}
+}
+
+func BenchmarkBinarySearch(b *testing.B) {
+	const n = 10000
+	A := make([]int, n)
+	for i := 0; i < n; i++ {
+		A[i] = i
+	}
+	for i:= 0; i< b.N; i++ {
+		BinarySearch(A, n)
+	}
+}
