2eea5d16d4fbafe099ee10e1fd8feb8db0b0f919
luoheng <1301089462@qq.com>
2019-10-22 15:32:54 +0800

    fix for bag 01 problem

--- algorithm-competition/chapter2/2.3.1.go
+++ algorithm-competition/chapter2/2.3.1.go
@@ -4,10 +4,8 @@ package chapter2
 func bag01(weight []int, value []int, totalW int) int {
 	dp := make([]int, totalW+1)
 	for i := 0; i < len(weight); i++ {
-		for j := totalW; j >= 0; j-- {
-			if j >= weight[i] && dp[j-weight[i]]+value[i] > dp[j] {
-				dp[j] = dp[j-weight[i]] + value[i]
-			}
+		for j := totalW; j >= weight[i]; j-- {
+			dp[j] = Max(dp[j], dp[j-weight[i]]+value[i])
 		}
 	}
 	return dp[totalW]
